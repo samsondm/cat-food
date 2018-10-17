af7c3cf14a690a92e5da47521f1b2e97b5385154
Dmitry <41114590+samsondm@users.noreply.github.com>
2018-10-17 13:58:00 +0300

    Updates

---
+++ precache-manifest.7fb77caca94d64fbc90dfde9c7f8334f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9385573779bc5985210fa77366cbb75f",
+    "url": "/cat-food/static/media/cat.93855737.png"
+  },
+  {
+    "revision": "4100b162ca0970d6312244b5ea936459",
+    "url": "/cat-food/static/media/background.4100b162.png"
+  },
+  {
+    "revision": "d9d2ef1a92f5192b3a379dde277c4868",
+    "url": "/cat-food/static/media/Exo2.0-Thin.d9d2ef1a.eot"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/cat-food/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "126ef4b9ed4f5b1d87bb",
+    "url": "/cat-food/static/js/runtime~main.126ef4b9.js"
+  },
+  {
+    "revision": "b8e2d836c7489fd16801",
+    "url": "/cat-food/static/js/main.b8e2d836.chunk.js"
+  },
+  {
+    "revision": "0915f8874b1a24b7a853",
+    "url": "/cat-food/static/js/1.0915f887.chunk.js"
+  },
+  {
+    "revision": "b8e2d836c7489fd16801",
+    "url": "/cat-food/static/css/main.04d921c8.chunk.css"
+  },
+  {
+    "revision": "2dfcff0d3305fdb505b756a44548aaf1",
+    "url": "/cat-food/index.html"
+  }
+];
